a18b16a90830fe1a156e789040753798e1d3e130
AbtinParvinroo <abtin83parvinroo@gmail.com>
2025-05-22 00:44:47 +0400

    Delete zeroPadding.py

--- zeroPadding.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def reflectPadding(img, pad):
-    paddedImg = np.pad(img, ((pad, pad), (pad, pad), (0, 0)), mode='reflect')
-    return paddedImg
-
-# h(j) = h + 2(padding)
-# w(i) = h + 2(padding)
