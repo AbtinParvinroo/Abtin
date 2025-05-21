14e018db7e806803fac7f22353ed9a53a925b965
AbtinParvinroo <abtin83parvinroo@gmail.com>
2025-05-22 00:43:45 +0400

    Delete dynamicBias.py

--- dynamicBias.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def dynamic_bias_layer(input_feature_map, bias_weights):
-    H, W, C_out = input_feature_map.shape
-    assert bias_weights.shape[0] == C_out
-    bias_reshaped = bias_weights.reshape(1, 1, C_out)
-    output = input_feature_map + bias_reshaped
-    return output
